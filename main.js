a976cfa4fdabdacc709cdb058e8fc67d45b311ab
Cephas Mutua <cephasmutua1984@gmail.com>
2022-09-15 22:01:40 +0300

    Initialize files & set up UI design

---
+++ main.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById('canvas'), 
+pickedColor = document.getElementById('picked-color'),
+black = document.getElementById('black'),
+random = document.getElementById('random'),
+erase = document.getElementById('erase'),
+reset = document.getElementById('reset'),
+sizeInput = document.getElementById('size-input'),
+body = document.querySelector('body');
+let selectedColor = ''
+
+
+
